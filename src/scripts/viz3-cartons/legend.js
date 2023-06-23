88316d64df3b5e436308f8650129271854f8e699
NaserHamitou <nas.hmt44@gmail.com>
2023-06-23 18:55:46 -0400

    viz 2 complet

--- src/scripts/viz3-cartons/legend.js
+++ src/scripts/viz3-cartons/legend.js
@@ -1,19 +1,41 @@
 export function drawLegend(){
-    const linear = d3.scaleLinear()
-    .domain([0,2])
-    .range(["rgb(46, 73, 123)", "rgb(71, 187, 94)"]);
 
-    const graph = d3.select("svg");
+    const legendData = [
+        { label: "Carton rouge", color: "red" },
+        { label: "Carton jaune", color: "yellow" },
+      ];
 
-    graph.append("g")
-    .attr("class", "legendLinear")
-    .attr("transform", "translate(20,20)");
+    const svg = d3.select('svg')
 
-    const legendLinear = d3.legendColor()
-    .shapeWidth(30)
-    .orient('horizontal')
-    .scale(linear);
 
-    graph.select(".legendLinear")
-    .call(legendLinear);
+
+    const legend = svg.append("g")
+        .attr('class', 'legend-container')
+        .selectAll(".legend")
+        .data(legendData)
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) {
+            return "translate(" + (275 + (i * 150)) + ", 0)";
+        });
+
+    legend.append("rect")
+        .attr("x", 10) 
+        .attr("y", 10)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function(d) {
+            return d.color;
+        });
+    
+    legend.append("text")
+        .attr("x", 40) 
+        .attr("y", 19) 
+        .attr("dy", ".35em")
+        .text(function(d) {
+            return d.label;
+        });
+
+        
 }
