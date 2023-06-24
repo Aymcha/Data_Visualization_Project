f3fb4918096edb8bec84248205ea82a8c906d8c5
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-06-23 19:02:35 -0700

    Create result.js

---
+++ src/scripts/viz1/result.js
@@ -0,0 +1,15 @@
+import * as viz from './scripts/viz1/viz.js.js'
+var width = 600
+var height = 400
+
+// Création de l'élément SVG
+var svg = d3.select('#chart-container')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+// Chargement des données depuis le fichier CSV
+d3.csv('./Résultats.csv')
+  .then(function (data) {
+    viz.createGraph(svg, data)
+  })
