a8d4fcea286b9e4fe8196c665bbdabe1a88152fa
ahmedsidi9 <70984071+ahmedsidi9@users.noreply.github.com>
2023-06-23 18:40:30 -0700

    integration v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import * as offensif from './scripts/viz6-offensif/offensive-metrics'
 
+import * as heatmap from './scripts/viz4-performance/performance.js'
+
 offensif.buildBarPlots()
+
+heatmap.buildHeatmap()
+
